f1f9977385b3a3eb366a46b44a524d16789ec454
JONAS ARAUJO RITTER <jonas_ritter@yahoo.com.br>
2019-06-21 01:32:39 -0300

    Remoção relatorios

--- web/js/dashboard.js
+++ web/js/dashboard.js
@@ -3,24 +3,6 @@ function ViewModel(){
 
 	self.list = ko.observableArray();
 
-	self.generateReport = function(data){
-		var dataFormated = ko.utils.arrayMap(data(), function(item){
-			var itemresponse = {};
-			if(item.epi) itemresponse['EPI'] = item.epi;
-			if(item.machine) itemresponse['Máquina'] = item.machine;
-			if(item.name) itemresponse['Funcionário'] = item.name;
-			if(item.area) itemresponse['Setor'] = item.area;
-			if(item.date) itemresponse['Data'] = item.date;
-			if(item.description) itemresponse['Irregularidade'] = item.description;
-			if(item.status) itemresponse['Status'] = item.status == 1 ? 'Resolver' : 'Resolvido';
-			return itemresponse;
-		});
-		var ws = XLSX.utils.json_to_sheet(dataFormated);
-		var wb = XLSX.utils.book_new();
-		XLSX.utils.book_append_sheet(wb, ws, "Dados");
-		XLSX.writeFile(wb, "Relatorio.xlsx");
-	}
-
 	self.getEpis = function(epis){
 		return ko.utils.arrayMap(epis, function(item){
 			return item.epi.nome;
