87d35485a8a049797bcf273c75e7b044f9b35b23
JONAS ARAUJO RITTER <jonas_ritter@yahoo.com.br>
2019-06-23 15:19:35 -0300

    IMplementação de login

--- web/js/funcionarios.js
+++ web/js/funcionarios.js
@@ -145,6 +145,10 @@ function ViewModel() {
     }
 }
 
+if(!localStorage.getItem("isAutenticated")){
+	window.location.replace('login.html');
+}
+
 var viewModel = new ViewModel();
 viewModel.getData();
 
